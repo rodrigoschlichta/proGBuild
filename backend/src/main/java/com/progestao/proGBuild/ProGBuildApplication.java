886da47efa21900045dd8a233076a9557a5a6a2b
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2024-12-17 15:31:24 -0300

    first commit~

---
+++ backend/src/main/java/com/progestao/proGBuild/ProGBuildApplication.java
@@ -0,0 +1,13 @@
+package com.progestao.proGBuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProGBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProGBuildApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/progestao/proGBuild/ProGBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progestao.proGBuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProGBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
